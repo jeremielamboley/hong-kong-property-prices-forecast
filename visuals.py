b865b9d1b5ece8481648c3eaf4d4d174eaa34106
Jeremie Lamboley <43125035+jeremielamboley@users.noreply.github.com>
2019-04-26 18:06:38 +0800

    added-models-evaluation

--- visuals.py
+++ visuals.py
@@ -15,6 +15,10 @@ from sklearn.model_selection import learning_curve
 from sklearn.model_selection import validation_curve
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import ShuffleSplit, train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def ModelLearning(X, y):
     """ Calculates the performance of several models with varying sizes of training data.
@@ -129,3 +133,144 @@ def PredictTrials(X, y, fitter, data):
 
     # Display price range
     print("\nRange in prices: ${:,.2f}".format(max(prices) - min(prices)))
+
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between true and predicted values based on the metric chosen. """
+    
+    # TODO: Calculate the performance score between 'y_true' and 'y_predict'
+    score = r2_score(y_true, y_predict)
+    
+    # Return the score
+    return score
+	
+def fit_model(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a decision tree regressor trained on the input data [X, y]. """
+    
+    # Create cross-validation sets from the training data
+    cv_sets = ShuffleSplit(n_splits = 10, test_size = 0.20, random_state = 0)
+
+    # Create a decision tree regressor object
+    regressor = DecisionTreeRegressor()
+
+    # Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    params = {'max_depth':[1,2,3,4,5,6,7,8,9,10,10000]}
+
+    # Transform 'performance_metric' into a scoring function using 'make_scorer' 
+    scoring_fnc = make_scorer(performance_metric)
+
+    # Create the grid search cv object --> GridSearchCV()
+    # Make sure to include the right parameters in the object:
+    # (estimator, param_grid, scoring, cv) which have values 'regressor', 'params', 'scoring_fnc', and 'cv_sets' respectively.
+    grid = GridSearchCV(estimator=regressor, param_grid=params, scoring=scoring_fnc, cv=cv_sets)
+
+    # Fit the grid search object to the data to compute the optimal model
+    grid = grid.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return grid.best_estimator_
+	
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+	
+def arima(pdq,seasonal_pdq):
+	for param in pdq:
+		for param_seasonal in seasonal_pdq:
+			try:
+				mod = sm.tsa.statespace.SARIMAX(y,
+												order=param,
+												seasonal_order=param_seasonal,
+												enforce_stationarity=False,
+												enforce_invertibility=False)
+				results = mod.fit()
+				print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+			except:
+				continue
+				
+
+def plot_learning_curves(estimator, train_X, train_y, val_X, val_y,suptitle='', title='', xlabel='', ylabel=''):
+    """
+    Plots learning curves for a given estimator.
+    Parameters
+    ----------
+    estimator : sklearn estimator
+    train_X : pd.DataFrame
+        training set (features)
+    train_y : pd.Series
+        training set (response)
+    val_X : pd.DataFrame
+        validation set (features)
+    val_y : pd.Series
+        validation set (response)
+    suptitle : str
+        Chart suptitle
+    title: str
+        Chart title
+    xlabel: str
+        Label for the X axis
+    ylabel: str
+        Label for the y axis
+    Returns
+    -------
+    Plot of learning curves
+    """
+    
+    # create lists to store train and validation scores
+    train_score = []
+    val_score = []
+
+    # create ten incremental training set sizes
+    training_set_sizes = np.linspace(5, len(train_X), 10, dtype='int')
+
+    # for each one of those training set sizes
+    for i in training_set_sizes:
+        # fit the model only using that many training examples
+        estimator.fit(train_X.iloc[0:i, :], train_y.iloc[0:i])
+        # calculate the training accuracy only using those training examples
+        train_accuracy = metrics.accuracy_score(
+                                    train_y.iloc[0:i],
+                                    estimator.predict(train_X.iloc[0:i, :])
+                                    )
+        # calculate the validation accuracy using the whole validation set
+        val_accuracy = metrics.accuracy_score(
+                                    val_y,
+                                    estimator.predict(val_X)
+                                    )
+        # store the scores in their respective lists
+        train_score.append(train_accuracy)
+        val_score.append(val_accuracy)
+        
+    # plot learning curves
+    fig, ax = plt.subplots(figsize=(14, 9))
+    ax.plot(training_set_sizes, train_score, c='gold')
+    ax.plot(training_set_sizes, val_score, c='steelblue')
+
+    # format the chart to make it look nice
+    fig.suptitle(suptitle, fontweight='bold', fontsize='20')
+    ax.set_title(title, size=20)
+    ax.set_xlabel(xlabel, size=16)
+    ax.set_ylabel(ylabel, size=16)
+    ax.legend(['training set', 'validation set'], fontsize=16)
+    ax.tick_params(axis='both', labelsize=12)
+    ax.set_ylim(0, 1)
+
+    def percentages(x, pos):
+        """The two args are the value and tick position"""
+        if x < 1:
+            return '{:1.0f}'.format(x*100)
+        return '{:1.0f}%'.format(x*100)
+
+    def numbers(x, pos):
+        """The two args are the value and tick position"""
+        if x >= 1000:
+            return '{:1,.0f}'.format(x)
+        return '{:1.0f}'.format(x)
+
+    y_formatter = FuncFormatter(percentages)
+    ax.yaxis.set_major_formatter(y_formatter)
+
+    x_formatter = FuncFormatter(numbers)
+    ax.xaxis.set_major_formatter(x_formatter)
